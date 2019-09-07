a9cfa94c97d1be075d8f3c861be9ffe6265309b4
Pavel K <pavelsc@gmail.com>
2019-09-07 19:20:34 +0300

    Undefined $extras variable
    Undefined $extras variable now is set to null

--- library/Zend/View/Helper/HeadLink.php
+++ library/Zend/View/Helper/HeadLink.php
@@ -399,6 +399,8 @@ class Zend_View_Helper_HeadLink extends Zend_View_Helper_Placeholder_Container_S
         if(0 < count($args) && is_array($args[0])) {
             $extras = array_shift($args);
             $extras = (array) $extras;
+        } else {
+	        $extras = null;
         }
 
         $attributes = compact('rel', 'type', 'href', 'media', 'conditionalStylesheet', 'extras');

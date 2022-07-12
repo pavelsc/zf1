6cba1c50987e8c26521c6acd467e2bd67c7b2dc6
Pavel K <p.kukin@tcp-soft.com>
2022-07-12 19:23:07 +0300

    Zend_Route fix

--- library/Zend/Controller/Action/HelperBroker/PriorityStack.php
+++ library/Zend/Controller/Action/HelperBroker/PriorityStack.php
@@ -88,7 +88,7 @@ class Zend_Controller_Action_HelperBroker_PriorityStack implements IteratorAggre
      *
      * @return array
      */
-    public function getIterator()
+    public function getIterator(): Traversable
     {
         return new ArrayObject($this->_helpersByPriority);
     }
@@ -99,7 +99,7 @@ class Zend_Controller_Action_HelperBroker_PriorityStack implements IteratorAggre
      * @param int|string $priorityOrHelperName
      * @return Zend_Controller_Action_HelperBroker_PriorityStack
      */
-    public function offsetExists($priorityOrHelperName)
+    public function offsetExists($priorityOrHelperName): bool
     {
         if (is_string($priorityOrHelperName)) {
             return array_key_exists($priorityOrHelperName, $this->_helpersByNameRef);
@@ -172,7 +172,7 @@ class Zend_Controller_Action_HelperBroker_PriorityStack implements IteratorAggre
      * @param int|string $priorityOrHelperName Priority integer or the helper name
      * @return Zend_Controller_Action_HelperBroker_PriorityStack
      */
-    public function offsetUnset($priorityOrHelperName)
+    #[ReturnTypeWillChange] public function offsetUnset($priorityOrHelperName)
     {
         if (!$this->offsetExists($priorityOrHelperName)) {
             require_once 'Zend/Controller/Action/Exception.php';
@@ -198,7 +198,7 @@ class Zend_Controller_Action_HelperBroker_PriorityStack implements IteratorAggre
      *
      * @return int
      */
-    public function count()
+    public function count(): int
     {
         return count($this->_helpersByPriority);
     }

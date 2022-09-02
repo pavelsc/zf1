200c172b369ec2c88f331431d713409b740ac39f
Pavel K <p.kukin@tcp-soft.com>
2022-09-02 13:52:49 +0300

    fix Argument #1 ($value) must be of type Countable|array, null given

--- library/Zend/Validate/File/Upload.php
+++ library/Zend/Validate/File/Upload.php
@@ -223,7 +223,7 @@ class Zend_Validate_File_Upload extends Zend_Validate_Abstract
             }
         }
 
-        if (count($this->_messages) > 0) {
+        if (is_array($this->_messages) && count($this->_messages) > 0) {
             return false;
         } else {
             return true;
